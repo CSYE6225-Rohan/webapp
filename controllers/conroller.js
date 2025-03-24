648c74e8a3295ceb91078dc0f9bbab8386c3b6ec
rohanjauhari1312 <jauhari.r@northeastern.edu>
2025-03-24 16:59:38 -0400

    fixing s3 connection

--- controllers/conroller.js
+++ controllers/conroller.js
@@ -7,8 +7,6 @@ const logger = require('./logger');
 
 const cloudwatch = new AWS.CloudWatch({ region: process.env.AWS_REGION });
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION
 });
 
